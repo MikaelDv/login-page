c069d5163da967a3076e32c5fb7109a39a28b4ab
MikaelDv <enzomikael@hotmail.com>
2024-06-11 01:55:31 -0300

    animações e verificação de login adicionados

--- js/login.js
+++ js/login.js
@@ -1,18 +1,57 @@
-const mostrarSenha = document.getElementById("lock");
-const inputSenha = document.getElementById("password");
+const showPassword = document.getElementById("lock");
+const inputPassword = document.getElementById("password");
+const inputUser = document.getElementById("username");
+const form = document.getElementById("loginForm");
 
-mostrarSenha.addEventListener("mouseenter", () => {
-  mostrarSenha.style.cursor = "pointer";
+const userList = [
+  { user: "Mikael", password: "mikaelamajujuba02" },
+  { user: "adm", password: "010203" },
+];
+
+showPassword.addEventListener("mouseenter", () => {
+  showPassword.style.cursor = "pointer";
 });
 
-mostrarSenha.addEventListener("click", () => {
-  if (mostrarSenha.classList.contains("bx-lock-alt")) {
-    mostrarSenha.classList.remove("bx-lock-alt");
-    mostrarSenha.classList.add("bx-lock-open-alt");
-    inputSenha.type = "text";
+showPassword.addEventListener("click", () => {
+  if (showPassword.classList.contains("bx-lock-alt")) {
+    showPassword.classList.remove("bx-lock-alt");
+    showPassword.classList.add("bx-lock-open-alt");
+    inputPassword.type = "text";
   } else {
-    mostrarSenha.classList.remove("bx-lock-open-alt");
-    mostrarSenha.classList.add("bx-lock-alt");
-    inputSenha.type = "password";
+    showPassword.classList.remove("bx-lock-open-alt");
+    showPassword.classList.add("bx-lock-alt");
+    inputPassword.type = "password";
   }
+});
+
+function verifyCredentials() {
+  for (let x = 0; x < userList.length; x++) {
+    if (
+      userList[x].user === inputUser.value &&
+      userList[x].password === inputPassword.value
+    ) {
+      return true;
+    } else {
+    }
+  }
+  return false;
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (verifyCredentials()) {
+    alert("Login efetuado com sucesso!");
+  } else {
+    form.classList.add("shake");
+    inputPassword.parentElement.classList.add("inptError");
+    inputUser.parentElement.classList.add("inptError");
+
+    setTimeout(() => form.classList.remove("shake"), 500);
+
+    setTimeout(() => {
+      inputPassword.parentElement.classList.remove("inptError");
+      inputUser.parentElement.classList.remove("inptError");
+    }, 600);
+  }
+  form.reset();
 });
